60b1c02d2afc5d99a792eaef2f5323b3fceb72ea
Cori Fowler <fowler.cori@gmail.com>
2016-02-08 15:46:19 -0500

    intial commit

---
+++ gulpfile.js
@@ -0,0 +1,146 @@
+var gulp       = require('gulp');
+var ts         = require('gulp-typescript');
+var tslint     = require('gulp-tslint');
+var sass       = require('gulp-sass');
+var plumber    = require('gulp-plumber');
+var server     = require('gulp-server-livereload');
+var jshint     = require('gulp-jshint');
+var stylish    = require('jshint-stylish');
+var htmlhint   = require('gulp-htmlhint');
+var sourcemaps = require('gulp-sourcemaps');
+var concat     = require('gulp-concat');
+
+var notifyError = function() {
+  return plumber({
+    errorHandler: notify.onError('Error: <%= error.message %>')
+  });
+};
+
+var paths = {
+  appJavascript: ['**/*.ts', '**/**/*.ts', '!node_modules/**/*.**']
+}
+
+// ============================================
+//  TYPESCRIPT LINTER
+// ============================================
+
+gulp.task('ts-lint', function() {
+ return gulp.src(paths.appJavascript)
+    .pipe(tslint())
+    .pipe(tslint.report('ts-report'));
+});
+
+
+// ============================================
+//  TYPESCRIPT TRANSPILING
+// ============================================
+
+// loads configuration
+
+var tsProject = ts.createProject('tsconfig.json'); 
+
+// loads all files from paths specification and transpiles files into js
+
+gulp.task('ts', function() {
+  var tsResults = tsProject.src(paths.appJavascript)
+    .pipe(ts(tsProject));
+
+  return tsResults.js.pipe(gulp.dest('./build/')); // save files in same place as .ts files
+});
+
+// ============================================
+//  CONCAT - take all transpiled to one .js
+// ============================================
+
+gulp.task('concat', function() {
+  return gulp.src([
+    './build/*.js',
+    './build/**/*.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./app/dist/'));
+});
+
+// ============================================
+//  SOURCEMAPS (JS & CSS)
+// ============================================
+
+gulp.task('js-map', function() {
+  gulp.src('./app/dist/app.js')
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/maps'));
+});
+
+gulp.task('css-map', function() {
+  gulp.src('./app/css/main.css')
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/maps'));
+});
+
+gulp.task('maps', ['js-map', 'css-map']);
+
+// ============================================
+//  COMPILE ALL THE THINGS
+// ============================================
+
+gulp.task('compile', ['concat', 'maps']);
+
+// ============================================
+//  SASS
+// ============================================
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/main.scss')
+    .pipe(notifyError())
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css'));
+});
+
+// ============================================
+//  WATCH
+// ============================================
+
+// start server + watch task
+
+gulp.task('watch', ['watchlist', 'webserver', 'compile']);
+
+gulp.task('watchlist', function() {
+  gulp.watch('./ts/**.ts', ['ts-lint']);
+  gulp.watch('./ts/**.ts', ['ts']);
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./app/index.html', ['hint:html']);
+  gulp.watch(['./build/**/*.js', '!./app/dist'], ['hint:js']);
+})
+
+// ============================================
+//  SERVER
+// ============================================
+
+gulp.task('webserver', function() {
+  return gulp.src('app')
+    .pipe(server({
+      livereload: true
+    }));
+});
+
+// ============================================
+//  HINT
+// ============================================
+
+gulp.task('hint:js', function() {
+  return gulp.src(['./build/*.js', './build/**/*.js', '!./app/dist/app.js'])
+    .pipe(notifyError())
+    .pipe(jshint({"esnext" : true}))
+    .pipe(jshint.reporter('fail'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('hint:html', function() {
+  return gulp.src('app/index.html')
+    .pipe(notifyError())
+    .pipe(htmlhint())
+    .pipe(htmlhint.failReporter());
+});
+
