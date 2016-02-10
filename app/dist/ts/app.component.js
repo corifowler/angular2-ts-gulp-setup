16b8d275be6679e72c515b58fff97e843593bd31
Cori Fowler <fowler.cori@gmail.com>
2016-02-10 14:59:04 -0500

    completed ng-heroes tutorial with router portion

--- app/dist/ts/app.component.js
+++ app/dist/ts/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router', './summary.component', './analytics.component', './demographic-profile.component', './disposition-profile.component', './performance-summary.component', './survey-projects.component'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './hero.service', './heroes.component', './dashboard.component', './hero-detail.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(['angular2/core', 'angular2/router', './summary.component', './a
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1, summary_component_1, analytics_component_1, demographic_profile_component_1, disposition_profile_component_1, performance_summary_component_1, survey_projects_component_1;
+    var core_1, router_1, hero_service_1, heroes_component_1, dashboard_component_1, hero_detail_component_1;
     var AppComponent;
     return {
         setters:[
@@ -18,42 +18,49 @@ System.register(['angular2/core', 'angular2/router', './summary.component', './a
             function (router_1_1) {
                 router_1 = router_1_1;
             },
-            function (summary_component_1_1) {
-                summary_component_1 = summary_component_1_1;
+            function (hero_service_1_1) {
+                hero_service_1 = hero_service_1_1;
             },
-            function (analytics_component_1_1) {
-                analytics_component_1 = analytics_component_1_1;
+            function (heroes_component_1_1) {
+                heroes_component_1 = heroes_component_1_1;
             },
-            function (demographic_profile_component_1_1) {
-                demographic_profile_component_1 = demographic_profile_component_1_1;
+            function (dashboard_component_1_1) {
+                dashboard_component_1 = dashboard_component_1_1;
             },
-            function (disposition_profile_component_1_1) {
-                disposition_profile_component_1 = disposition_profile_component_1_1;
-            },
-            function (performance_summary_component_1_1) {
-                performance_summary_component_1 = performance_summary_component_1_1;
-            },
-            function (survey_projects_component_1_1) {
-                survey_projects_component_1 = survey_projects_component_1_1;
+            function (hero_detail_component_1_1) {
+                hero_detail_component_1 = hero_detail_component_1_1;
             }],
         execute: function() {
-            // skeleton setup
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'Tour of Heroes';
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>angular2 app</h1>\n    <nav>\n      <a [routerLink]=\"['Summary']\">Summary</a>\n      <a [routerLink]=\"['Analytics']\">Analytics</a>\n      <a [routerLink]=\"['DispositionProfile']\">Disposition Profile</a>\n      <a [routerLink]=\"['DemographicProfile']\">Demographic Profile</a>\n      <a [routerLink]=\"['PerformanceSummary']\">Performance Summary</a>\n      <a [routerLink]=\"['SurveyProjects']\">Survey Projects</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
-                        directives: [router_1.ROUTER_DIRECTIVES]
+                        template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a [routerLink]=\"['Dashboard']\">Dashboard</a>\n      <a [routerLink]=\"['Heroes']\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
+                        styleUrls: ['app/css/app.component.css'],
+                        directives: [router_1.ROUTER_DIRECTIVES],
+                        providers: [
+                            hero_service_1.HeroService,
+                            router_1.ROUTER_PROVIDERS
+                        ]
                     }),
                     router_1.RouteConfig([
-                        { path: '/summary', name: 'Summary', component: summary_component_1.SummaryComponent },
-                        { path: '/analytics', name: 'Analytics', component: analytics_component_1.AnalyticsComponent },
-                        { path: '/disposition-profile', name: 'DispositionProfile', component: disposition_profile_component_1.DispositionProfileComponent },
-                        { path: '/demographic-profile', name: 'DemographicProfile', component: demographic_profile_component_1.DemographicProfileComponent },
-                        { path: '/performance-summary', name: 'PerformanceSummary', component: performance_summary_component_1.PerformanceSummaryComponent },
-                        { path: '/survey-projects', name: 'SurveyProjects', component: survey_projects_component_1.SurveyProjectsComponent }
+                        {
+                            path: '/heroes',
+                            name: 'Heroes',
+                            component: heroes_component_1.HeroesComponent
+                        }, {
+                            path: '/dashboard',
+                            name: 'Dashboard',
+                            component: dashboard_component_1.DashboardComponent,
+                            useAsDefault: true
+                        }, {
+                            path: '/detail/:id',
+                            name: 'HeroDetail',
+                            component: hero_detail_component_1.HeroDetailComponent
+                        }
                     ]), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- ts/app.component.ts
+++ ts/app.component.ts
@@ -1,41 +1,46 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
-import {SummaryComponent} from './summary.component';
-import {AnalyticsComponent} from './analytics.component';
-import {DemographicProfileComponent} from './demographic-profile.component';
-import {DispositionProfileComponent} from './disposition-profile.component';
-import {PerformanceSummaryComponent} from './performance-summary.component';
-import {SurveyProjectsComponent} from './survey-projects.component';
-
-// skeleton setup
+import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>angular2 app</h1>
+    <h1>{{title}}</h1>
     <nav>
-      <a [routerLink]="['Summary']">Summary</a>
-      <a [routerLink]="['Analytics']">Analytics</a>
-      <a [routerLink]="['DispositionProfile']">Disposition Profile</a>
-      <a [routerLink]="['DemographicProfile']">Demographic Profile</a>
-      <a [routerLink]="['PerformanceSummary']">Performance Summary</a>
-      <a [routerLink]="['SurveyProjects']">Survey Projects</a>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
     </nav>
     <router-outlet></router-outlet>
   `,
-  directives: [ROUTER_DIRECTIVES]
+  styleUrls: ['app/css/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HeroService, 
+    ROUTER_PROVIDERS
+  ]
 })
 
-// different routes for app
-
 @RouteConfig([
-    { path: '/summary',             name: 'Summary',            component: SummaryComponent },
-    { path: '/analytics',           name: 'Analytics',          component: AnalyticsComponent },
-    { path: '/disposition-profile', name: 'DispositionProfile', component: DispositionProfileComponent },
-    { path: '/demographic-profile', name: 'DemographicProfile', component: DemographicProfileComponent },
-    { path: '/performance-summary', name: 'PerformanceSummary', component: PerformanceSummaryComponent },
-    { path: '/survey-projects',     name: 'SurveyProjects',     component: SurveyProjectsComponent }
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }, {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }, {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  }
 ])
 
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
